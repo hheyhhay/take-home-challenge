553c79d9ad4b60cf1e6d3d1bcd58e67ba12ff0d9
Hayley Dixon <hdixon37@gmail.com>
2021-11-03 15:57:37 -0600

    Add data cleaning file to add id and minimize data

---
+++ src/util/dataCleaning.js
@@ -0,0 +1,16 @@
+export const dataCleaning = (data) => {
+
+  let dataArray = data.results.map(article => {
+    let simplifiedData = {};
+
+    simplifiedData.id = Date.now();
+    simplifiedData.author = article.byline;
+    simplifiedData.images = article.multimedia;
+    simplifiedData.publishedDate = article.published_date;
+    simplifiedData.shortURL = article.short_url;
+    simplifiedData.title = article.title;
+    simplifiedData.abstract = article.abstract;
+    return simplifiedData
+  })
+return dataArray;
+}
